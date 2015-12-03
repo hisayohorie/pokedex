a3cadb2f5910405c769b6e3ee4128635a295e758
Mina Mikhail <mina@fightingtheboss.com>
2015-12-03 11:17:09 -0500

    Initial commit for Pokedex

---
+++ db/migrate/20151203161647_create_pokemons.rb
@@ -0,0 +1,15 @@
+class CreatePokemons < ActiveRecord::Migration
+  def change
+    create_table :pokemons do |t|
+      t.string :name
+      t.integer :attack
+      t.integer :defense
+      t.integer :happiness
+      t.integer :speed
+      t.string :species
+      t.string :growth_rate
+
+      t.timestamps null: false
+    end
+  end
+end
