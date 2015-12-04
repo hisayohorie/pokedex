dda2f63fee49f178297a272c92c35fddbc3e9fe7
Mina Mikhail <mina@fightingtheboss.com>
2015-12-04 11:03:27 -0500

    Add JSON responses, limit index attributes

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,9 +1,25 @@
 class PokemonsController < ApplicationController
   def index
-  	@pokemon = Pokemon.all
+  	@pokemons = Pokemon.all
+
+  	respond_to do |format|
+  		format.html
+  		format.json do
+  			# render json: @pokemons.map do |pokemon|
+  			# 	{ name: pokemon.name, image_url: pokemon.image_url }
+  			# end
+
+  			render json: @pokemons, only: [:name, :image_url]
+  		end
+  	end
   end
 
   def show
   	@pokemon = Pokemon.find(params[:id])
+
+  	respond_to do |format|
+  		format.html
+  		format.json { render json: @pokemon }
+  	end
   end
 end
